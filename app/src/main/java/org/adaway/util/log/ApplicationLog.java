8d42decb125b3eee2743ba5f2a263503c2bf2923
pumPCin <pumpcin14@mail.ru>
2026-03-06 20:08:43 +0300

    update

--- app/src/main/java/org/adaway/util/log/ApplicationLog.java
+++ app/src/main/java/org/adaway/util/log/ApplicationLog.java
@@ -8,8 +8,6 @@ import com.topjohnwu.superuser.Shell;
 
 import org.adaway.helper.PreferenceHelper;
 
-import timber.log.Timber;
-
 /**
  * This class is an utility class that configures the application log.
  *
@@ -29,16 +27,10 @@ public final class ApplicationLog {
      * @param application The application instance.
      */
     public static void init(Application application) {
-        if (isApplicationDebuggable(application) || PreferenceHelper.getDebugEnabled(application)) {
-            Shell.enableVerboseLogging = true;
-            Timber.plant(new Timber.DebugTree());
-        } else {
             Shell.enableVerboseLogging = false;
-            SentryLog.init(application);
-        }
     }
 
     private static boolean isApplicationDebuggable(Context context) {
-        return (context.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        return false;
     }
 }
